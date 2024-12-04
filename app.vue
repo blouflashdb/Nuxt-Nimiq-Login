4c8a4cd71a85aad9230a6e67fb679bfc6c9ce415
Daniel Bugaew <daniel.bugaew@bayoo.net>
2024-12-04 11:22:36 +0100

    initial working example

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data } = useFetch("/api/auth/challange")
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    {{ data }}
+  </div>
+</template>
---
+++ server/api/auth/verify.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+    const session = await useSession(event, {
+        password: "80d42cfb-1cd2-462c-8f17-e3237d9027e9",
+      });
+  
+    return session.data;
+})
